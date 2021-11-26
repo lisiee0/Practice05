0e92c21e28b94d4bb45e9aeea64a5a90128eed9e
Hojun_Yoo <lisiee0@naver.com>
2021-11-26 10:49:58 +0900

    ex09

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,17 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] strArray){
        
         //메소드 내용작성
+    	String print= "";
     	
+    	int i= 0; 
+    	do {   		
+    		print= print+strArray[i];
+    		i++;
+    	}
+    	while(i<strArray.length);
+    	return print;
     }
-    
 }
